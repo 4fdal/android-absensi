f133880d722d68faef74152da6ef50504c701402
my4fdal <my4fdal@gmail.com>
2020-02-18 11:27:28 +0700

    data

--- app/src/main/java/com/example/absensi/MainActivity.java
+++ app/src/main/java/com/example/absensi/MainActivity.java
@@ -83,6 +83,8 @@ public class MainActivity extends AppCompatActivity {
                         } else {
                             linkIntentAbsenHandler(nip);
                         }
+                    } else {
+                        Toast.makeText(getApplicationContext(), "MAAF NIP KAMI TIDAK TERDAFTAR SILAHKAN HUBUNGI STI", Toast.LENGTH_LONG).show();
                     }
 
                 } catch (JSONException e) {
